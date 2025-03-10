90423f8fe34afaa12faf83bec14091d3c375a6a5
AApoLLoo <pommier.antoine@icloud.com>
2025-03-10 18:29:43 +0100

    Personnage terminé

--- phaser_environnement_vide/src/js/Egypte.js
+++ phaser_environnement_vide/src/js/Egypte.js
@@ -10,17 +10,29 @@ export default class Egypte extends Phaser.Scene {
         this.load.spritesheet("player2", "src/assets/Personnage - Copie.png", { frameWidth: 80, frameHeight: 64 });
         this.load.spritesheet("pants", "src/assets/Pants.png", { frameWidth: 80, frameHeight: 64 });
         this.load.spritesheet("pants2", "src/assets/Pants - Copie.png", { frameWidth: 80, frameHeight: 64 });
+        this.load.spritesheet("shirt", "src/assets/Shirt.png", { frameWidth: 80, frameHeight: 64 });
+        this.load.spritesheet("shirt2", "src/assets/Shirt - Copie.png", { frameWidth: 80, frameHeight: 64 });
+        this.load.spritesheet("shoes", "src/assets/Shoes.png", { frameWidth: 80, frameHeight: 64 });
+        this.load.spritesheet("shoes2", "src/assets/Shoes - Copie.png", { frameWidth: 80, frameHeight: 64 });
     }
     create(){
         this.add.image(960, 540, "Ciel");
         this.player = this.physics.add.sprite(100, 450, "player");
         this.pants = this.physics.add.sprite(100, 450, "pants");
+        this.shirt = this.physics.add.sprite(100, 450, "shirt");
+        this.shoes = this.physics.add.sprite(100, 450, "shoes");
+        this.player.direction = 'right';
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
         this.pants.setBounce(0.2);
         this.pants.setCollideWorldBounds(true);
         this.pants.direction = 'right';
-        this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-        this.player.direction = 'right';
+        this.shirt.setBounce(0.2);
+        this.shirt.setCollideWorldBounds(true);
+        this.shirt.direction = 'right';
+        this.shoes.setBounce(0.2);
+        this.shoes.setCollideWorldBounds(true);
+        this.shoes.direction = 'right';
         this.anims.create({
             key: "anim_face",
             frames: [{ key: "player", frame: 4 }],
@@ -39,7 +51,7 @@ export default class Egypte extends Phaser.Scene {
         this.anims.create({
             key: "anim_saut",
             frames: this.anims.generateFrameNumbers("player", { start: 22, end: 24 }),
-            frameRate: 8,
+            frameRate: 4,
         });
         this.anims.create({
             key: "anim_face_pants",
@@ -59,7 +71,47 @@ export default class Egypte extends Phaser.Scene {
         this.anims.create({ 
             key: "anim_saut_pants",
             frames: this.anims.generateFrameNumbers("pants", { start: 22, end: 24 }),
+            frameRate: 4,
+        });
+        this.anims.create({
+            key: "anim_face_shirt",
+            frames: [{ key: "shirt", frame: 4 }],
+            frameRate: 20
+            });
+        this.anims.create({
+            key: "anim_tourne_gauche_shirt",
+            frames: this.anims.generateFrameNumbers("shirt", { start: 14, end: 16 }),
             frameRate: 8,
+        });
+        this.anims.create({
+            key: "anim_tourne_droite_shirt",
+            frames: this.anims.generateFrameNumbers("shirt2", { start: 14, end: 16}),
+            frameRate: 8,
+        });
+        this.anims.create({
+            key: "anim_saut_shirt",
+            frames: this.anims.generateFrameNumbers("shirt", { start: 22, end: 24 }),
+            frameRate: 4,
+        });
+        this.anims.create({
+            key: "anim_face_shoes",
+            frames: [{ key: "shoes", frame: 4 }],
+            frameRate: 20
+            });
+        this.anims.create({
+            key: "anim_tourne_gauche_shoes",
+            frames: this.anims.generateFrameNumbers("shoes", { start: 14, end: 16 }),
+            frameRate: 8,
+        });
+        this.anims.create({
+            key: "anim_tourne_droite_shoes",
+            frames: this.anims.generateFrameNumbers("shoes2", { start: 14, end: 16}),
+            frameRate: 8,
+        });
+        this.anims.create({
+            key: "anim_saut_shoes",
+            frames: this.anims.generateFrameNumbers("shoes", { start: 22, end: 24 }),
+            frameRate: 4,
         });
         clavier = this.input.keyboard.createCursorKeys();
     }
@@ -67,29 +119,49 @@ export default class Egypte extends Phaser.Scene {
         if (clavier.left.isDown) {
             this.player.direction = 'left';
             this.pants.direction = 'left';
+            this.shirt.direction = 'left';
+            this.shoes.direction = 'left';
             this.player.setVelocityX(-160);
             this.pants.setVelocityX(-160);
+            this.shirt.setVelocityX(-160);
+            this.shoes.setVelocityX(-160);
             this.player.anims.play("anim_tourne_gauche", true);
-            this.player.anims.play("anim_tourne_gauche_pants", true);
+            this.pants.anims.play("anim_tourne_gauche_pants", true);
+            this.shirt.anims.play("anim_tourne_gauche_shirt", true);
+            this.shoes.anims.play("anim_tourne_gauche_shoes", true);
           } else if (clavier.right.isDown) {
             this.player.direction = 'right';
             this.pants.direction = 'right';
+            this.shirt.direction = 'right';
+            this.shoes.direction = 'right';
             this.player.setVelocityX(160);
             this.pants.setVelocityX(160);
+            this.shirt.setVelocityX(160);
+            this.shoes.setVelocityX(160);
             this.player.anims.play("anim_tourne_droite", true);
-            this.player.anims.play("anim_tourne_droite_pants", true);
+            this.pants.anims.play("anim_tourne_droite_pants", true);
+            this.shirt.anims.play("anim_tourne_droite_shirt", true);
+            this.shoes.anims.play("anim_tourne_droite_shoes", true);
           } else {
             this.player.setVelocityX(0);
             this.pants.setVelocityX(0);
+            this.shirt.setVelocityX(0);
+            this.shoes.setVelocityX(0);
             this.player.anims.play("anim_face");
-            this.player.anims.play("anim_face_pants");
+            this.pants.anims.play("anim_face_pants");
+            this.shirt.anims.play("anim_face_shirt");
+            this.shoes.anims.play("anim_face_shoes");
           }
         
           if (clavier.up.isDown && (this.player.body.touching.down || this.player.body.blocked.down)) {
             this.player.anims.play("anim_saut", true);
-            this.player.anims.play("anim_saut", true);
+            this.pants.anims.play("anim_saut_pants", true);
+            this.shirt.anims.play("anim_saut_shirt", true);
+            this.shoes.anims.play("anim_saut_shoes", true);
             this.pants.setVelocityY(-330);
             this.player.setVelocityY(-330);
+            this.shirt.setVelocityY(-330);
+            this.shoes.setVelocityY(-330);
           } }
         
     }
