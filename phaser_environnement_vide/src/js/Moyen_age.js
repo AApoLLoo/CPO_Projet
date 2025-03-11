cb458c37cc191423d645a5d2db7230758a6ffa34
Ninon PREVOT <ninon.prevot@epfedu.fr>
2025-03-11 21:01:09 +0100

    mise en place du perso

--- phaser_environnement_vide/src/js/Moyen_age.js
+++ phaser_environnement_vide/src/js/Moyen_age.js
@@ -11,11 +11,11 @@ export default class Moyen_age extends Phaser.Scene {
     create(){
 
         const carteDuNiveau3 = this.add.tilemap("MAPmoyenage");   
-        const tileset = carteDuNiveau3.addTilesetImage( "Sans titre", "tuilemoyenage",32,32);  
-        const calque_background = carteDuNiveau3.createLayer("calque background", tileset);
+        const tileset = carteDuNiveau.addTilesetImage( "Sanstitre", "tuilemoyenage",32,32);  
+        const calque_background = carteDuNiveau.createLayer("calque_background", tileset);
         const calque_2 = carteDuNiveau3.createLayer("calque_2", tileset);
         const calque_3 = carteDuNiveau3.createLayer("calque_3", tileset);
-        const calque_4 = carteDuNiveau3.createLayer("calque_4", tileset);;
+        const calque_4 = carteDuNiveau3.createLayer("calque_4", tileset);
         calque_2.setCollisionByProperty({ estSolide: true }); 
         calque_3.setCollisionByProperty({ estSolide: true }); 
         calque_4.setCollisionByProperty({ estSolide: true }); 
