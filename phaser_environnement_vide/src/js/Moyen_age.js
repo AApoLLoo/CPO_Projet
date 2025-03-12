d3e2d52d51a2d63b67c77e857160bcc3aa843136
antoine458 <daniellstratu@gmail.com>
2025-03-12 16:51:45 +0100

    teleporter2 ajouté

--- phaser_environnement_vide/src/js/Moyen_age.js
+++ phaser_environnement_vide/src/js/Moyen_age.js
@@ -2,6 +2,8 @@ var clavier;
 var player;
 var score = 0;
 var zone_texte_score;
+var boutondoor;
+var TP;
 
 export default class Moyen_age extends Phaser.Scene {
     constructor() {
@@ -21,6 +23,9 @@ export default class Moyen_age extends Phaser.Scene {
     this.load.image("epee", "src/assets/epee.png"); // Ajoute l'image de l'épée
     this.load.image("HP", "src/assets/Coeur_HP.png");
    
+
+    //teleportation
+    this.load.spritesheet("TP", "src/assets/teleporter2.png", { frameWidth: 154, frameHeight: 148}); // Ajout gobelins
     
 
 
@@ -191,6 +196,31 @@ zone_texte_score = this.add.text(this.cameras.main.width / 2, 50, 'SCORE : 0', {
    fontStyle: 'bold',
    fontFamily: 'Times New Roman' // Remplacer ici par la police de ton choix
 }).setOrigin(0.5).setScrollFactor(0);
+
+
+//TELEPORTATION
+TP=this.physics.add.sprite(3700, 100, "TP");
+        TP.body.immovable = true;
+        TP.setAllowGravity = false;  
+        this.physics.add.collider(TP, calque_2);
+        this.anims.create({
+            key: 'teleporteur',
+            frames: this.anims.generateFrameNumbers('TP', { start: 0, end: 5 }),
+            frameRate: 4
+            ,
+            });
+
+            boutondoor= this.input.keyboard.addKey('F');
+
+
+
+
+
+
+
+
+
+
    }
 
     
@@ -241,7 +271,23 @@ zone_texte_score = this.add.text(this.cameras.main.width / 2, 50, 'SCORE : 0', {
 // Attaque du joueur
 if (Phaser.Input.Keyboard.JustDown(this.attackKey)) {
     this.attack();
+
 }
+
+// Téléportation
+if (boutondoor.isDown && this.physics.overlap(this.player, TP)) {
+    TP.anims.play('teleporteur', true);
+    
+}
+
+
+
+
+
+
+
+
+
 }
 
 
